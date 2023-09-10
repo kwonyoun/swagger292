335b08b40be5000c8058c6440efc2b652598096d
kwon <kyjyj6334@gmail.com>
2023-09-10 16:43:10 +0900

    first commit

---
+++ src/main/java/com/example/swagger292/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.swagger292;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Swagger292Application.class);
+	}
+
+}
