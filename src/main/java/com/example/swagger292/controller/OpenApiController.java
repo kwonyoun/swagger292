590f32c52ff293b57622be55fd4c8cba532377bf
kwon <kyjyj6334@gmail.com>
2023-09-12 17:33:32 +0900

    서울테니스장 오픈api insert 시도 중

--- src/main/java/com/example/swagger292/controller/OpenApiController.java
+++ src/main/java/com/example/swagger292/controller/OpenApiController.java
@@ -4,20 +4,18 @@ import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 
-import org.springframework.http.ResponseEntity;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.swagger292.api.OpenApiManager;
-import com.example.swagger292.service.BoardService;
-
-import lombok.extern.slf4j.Slf4j;
+import com.example.swagger292.dto.TennisDTO;
+import com.mysql.cj.xdevapi.JsonParser;
 
 @RestController
 @RequestMapping("open-api")
@@ -68,7 +66,7 @@ public class OpenApiController {
 		}
 		rd.close();
 		conn.disconnect();
-		System.out.println(sb.toString());
+		// System.out.println(sb.toString());
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -89,7 +87,7 @@ public class OpenApiController {
             urlBuilder.append("/" +  URLEncoder.encode("json","UTF-8") ); /*요청파일타입 (xml,xmlf,xls,json) */
             urlBuilder.append("/" + URLEncoder.encode("ListPublicReservationSport","UTF-8")); /*서비스명 (대소문자 구분 필수입니다.)*/
             urlBuilder.append("/" + URLEncoder.encode("1","UTF-8")); /*요청시작위치 (sample인증키 사용시 5이내 숫자)*/
-            urlBuilder.append("/" + URLEncoder.encode("2","UTF-8")); /*요청종료위치(sample인증키 사용시 5이상 숫자 선택 안 됨)*/
+            urlBuilder.append("/" + URLEncoder.encode("1","UTF-8")); /*요청종료위치(sample인증키 사용시 5이상 숫자 선택 안 됨)*/
             // 상위 5개는 필수적으로 순서바꾸지 않고 호출해야 합니다.
             
             // 서비스별 추가 요청 인자이며 자세한 내용은 각 서비스별 '요청인자'부분에 자세히 나와 있습니다.
@@ -107,6 +105,49 @@ public class OpenApiController {
                 result.append(returnLine);
             }
             jsonPrintString = result.toString();
+            
+            //build.gradle json추가
+            JSONObject json = new JSONObject(jsonPrintString);
+            System.out.println("json"+json);
+            String MAXCLASSNM = json.getJSONObject("ListPublicReservationSport").getJSONArray("row").getString(1);
+            System.out.println("체육시설 "+MAXCLASSNM);
+
+            JSONObject obj = new JSONObject(jsonPrintString);
+            JSONObject listPublicReservationSport = obj.getJSONObject("ListPublicReservationSport");
+            JSONArray row = listPublicReservationSport.getJSONArray("row");
+
+            for (int i = 0; i < row.length(); i++) {
+                String svcstatnm = row.getJSONObject(i).getString("SVCSTATNM"); // 서비스 상태
+                String usetgtinfo = row.getJSONObject(i).getString("USETGTINFO"); // 서비스 대상 정보
+                String gubun = row.getJSONObject(i).getString("GUBUN"); // 서비스 구분
+                String rcptenddt = row.getJSONObject(i).getString("RCPTENDDT"); // 접수 종료 일시
+                String svcopnenddt = row.getJSONObject(i).getString("SVCOPNENDDT"); // 서비스 개시 종료 일시
+                String svcid = row.getJSONObject(i).getString("SVCID"); // 서비스 ID
+                String v_max = row.getJSONObject(i).getString("V_MAX"); // 서비스 이용 종료 시간
+                String v_min = row.getJSONObject(i).getString("V_MIN"); // 서비스 이용 시작 시간
+                String telno = row.getJSONObject(i).getString("TELNO"); // 전화번호
+                String minclassnm = row.getJSONObject(i).getString("MINCLASSNM"); // 소분류명 
+                String payatnm = row.getJSONObject(i).getString("PAYATNM"); // 결제 방법
+                String svcurl = row.getJSONObject(i).getString("SVCURL"); // 바로가기 URL
+                String revstddaynm = row.getJSONObject(i).getString("REVSTDDAYNM"); // 취소 기간 기준 정보
+                String revstdday = row.getJSONObject(i).getString("REVSTDDAY"); // 취소 기간 기준일까지
+                String placenm = row.getJSONObject(i).getString("PLACENM"); // 장소명
+                String dtlcont = row.getJSONObject(i).getString("DTLCONT"); // 상세 정보
+                String areanm = row.getJSONObject(i).getString("AREANM"); // 지역명
+                String rcptbgndt = row.getJSONObject(i).getString("RCPTBGNDT"); // 접수 시작 일시
+                String svcopnbgndt = row.getJSONObject(i).getString("SVCONPBGNDT"); // 서비스 개시 시작 일시 
+                String svcnm = row.getJSONObject(i).getString("SVCNM");  // 서비스명 
+                String y = row.getJSONObject(i).getString("Y");  // 장소 Y 좌표 
+                String imgurl = row.getJSONObject(i).getString("IMGURL"); //이미지 경로
+                String maxclassnm = row.getJSONObject(i).getString("MAXCLASSNM"); //대분류명
+                String x = row.getJSONObject(i).getString("X"); //장소 X 좌표
+                // System.out.println("1: "+maxClassNm);
+                // System.out.println("2:"+SVCNM);
+                // 이제 maxClassNm 변수를 원하는 대로 사용할 수 있습니다.
+                // 예를 들어, ArrayList에 추가하거나 데이터베이스에 저장할 수 있습니다.
+                // TennisDTO.
+
+            }
 
 
         } catch (Exception e) {
---
+++ src/main/java/com/example/swagger292/dto/TennisDTO.java
@@ -0,0 +1,33 @@
+package com.example.swagger292.dto;
+
+import lombok.Data;
+
+@Data
+public class TennisDTO {
+
+    String svcstatnm;
+    String usetgtinfo;
+    String gubun;
+    int rcptenddt ;
+    int svcopnenddt ;
+    String svcid;
+    String v_max;
+    String v_min;
+    String telno;
+    String minclassnm;;
+    String payatnm;
+    String svcurl;;
+    String revstddaynm;
+    int revstdday;
+    String placenm;
+    String dtlcont;
+    String areanm;
+    int rcptbgndt;
+    int svcopnbgndt;
+    String svcnm;
+    String y;;
+    String imgurl;;
+    String maxclassnm;
+    String x;
+    
+}
